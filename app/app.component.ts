ebd250a7f4f63a068885f83edc530bc401806f5d
Kiattiphong Saetang <lengkubpome@hotmail.com>
2016-08-11 12:44:42 +0700

    add underscore and typings

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `<h1>My First Angular 2 App</h1>`
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+    a = ['moe', 'larry', 'curly', 'larry'];
+    b = [30, 40, 50, 30];
+    c = _.object(this.a, this.b);
+    oc = _.values(this.c);
+
+    ngOnInit() {
+        // console.log(_.keys(this.c));
+
+    }
+
+
+
+}
+
