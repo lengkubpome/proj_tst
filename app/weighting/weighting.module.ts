e4a7017cb59cfe7565f41ed2478f7c4175d023c3
Kiattiphong Saetang <lengkubpome@hotmail.com>
2016-08-15 17:52:40 +0700

    update Weighting-IN form

--- app/weighting/weighting.module.ts
+++ app/weighting/weighting.module.ts
@@ -3,16 +3,22 @@ import { CommonModule }   from '@angular/common';
 import { FormsModule }    from '@angular/forms';
 
 import { WeightingComponent } from './weighting.component';
+import { WeightingInFormComponent } from './weighting-in/weighting-in-form.component';
+
 import { WeightingService } from './weighting.service';
+
+import { weightingRouting } from './weighting.routing';
 
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    weightingRouting
   ],
   declarations: [
-    // WeightingComponent
+    WeightingComponent,
+    WeightingInFormComponent
   ],
   providers: [
     WeightingService
