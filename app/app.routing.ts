bf85900a080c9cf9642d53b07bcafc6b5d9fbe9b
Kiattiphong Saetang <lengkubpome@gmail.com>
2016-08-16 00:30:10 +0700

    Revert "update Weighting-IN form"
    This reverts commit e4a7017cb59cfe7565f41ed2478f7c4175d023c3.

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,10 +1,12 @@
 import { Routes, RouterModule } from '@angular/router';
 
+import { WeightingComponent } from './weighting/weighting.component';
 import { CustomersComponent } from './customers/customers.component';
 
 const appRoutes: Routes = [
+  { path: 'weighting', component: WeightingComponent },
   { path: 'customers', component: CustomersComponent },
-  { path: '**', component: CustomersComponent }
+  { path: '**', component: WeightingComponent }
 ];
 
 export const appRoutingProviders: any[] = [
--- app/weighting/weighting.module.ts
+++ app/weighting/weighting.module.ts
@@ -3,22 +3,16 @@ import { CommonModule }   from '@angular/common';
 import { FormsModule }    from '@angular/forms';
 
 import { WeightingComponent } from './weighting.component';
-import { WeightingInFormComponent } from './weighting-in/weighting-in-form.component';
-
 import { WeightingService } from './weighting.service';
-
-import { weightingRouting } from './weighting.routing';
 
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    weightingRouting
   ],
   declarations: [
-    WeightingComponent,
-    WeightingInFormComponent
+    // WeightingComponent
   ],
   providers: [
     WeightingService
