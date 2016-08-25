921e02af6da8931520d6eab9c8f68565a459b7b0
Kiattiphong Saetang <lengkubpome@hotmail.com>
2016-08-25 16:09:23 +0700

    revert

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,10 @@
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { disableDeprecatedForms, provideForms } from '@angular/forms';
 
 import { AppModule } from './app.module';
+
 
 platformBrowserDynamic().bootstrapModule(AppModule, [
     disableDeprecatedForms(),
--- app/weighting/weighting-in/weighting-in-form.component.ts
+++ app/weighting/weighting-in/weighting-in-form.component.ts
@@ -1,6 +1,6 @@
+/// <reference path="../../../typings/index.d.ts" />
 import { Component, OnInit, Output, EventEmitter} from '@angular/core';
 import {
-    REACTIVE_FORM_DIRECTIVES,
     FormBuilder,
     FormGroup,
     FormControl,
@@ -8,16 +8,12 @@ import {
 } from '@angular/forms';
 
 import { WeightingCar, WeightingService } from '../weighting.service';
+import { Customer, CustomersService } from '../../customers/customers.service';
 import { WeightingInFormValidators } from '../validations/weightingInFormValidators';
 import {Observable} from 'rxjs/Rx';
-<<<<<<< HEAD
-declare var $:JQueryStatic;
-=======
 
-<<<<<<< HEAD
->>>>>>> parent of 296507a... ...
-=======
->>>>>>> parent of 296507a... ...
+
+
 
 @Component({
     selector: 'weighting-in-form',
@@ -26,17 +22,18 @@ declare var $:JQueryStatic;
 })
 export class WeightingInFormComponent implements OnInit {
 
-    weightingInForm: FormGroup;
-
     @Output() save: EventEmitter<Object> = new EventEmitter(true);
     @Output() cancel = new EventEmitter();
 
+    customer: Customer;
+    weightingInForm: FormGroup;
     model = new WeightingCar(12, null, null, null, null, null, null, null);
-
     products = ['เหล็ก', 'กระดาษ', 'กระป๋อง', 'สังกะสี'];
 
     constructor(private _weightingService: WeightingService,
-        private _fb: FormBuilder) {
+        private _fb: FormBuilder,
+        private _customerService: CustomersService) {
+
         this.weightingInForm = this._fb.group({
             carId: ['', Validators.compose([
                 <any>Validators.required,
@@ -49,6 +46,8 @@ export class WeightingInFormComponent implements OnInit {
             product: ['', Validators.required],
             weight: ['', Validators.required]
         });
+
+        // var keyupCarId = Observable.fromEvent($("carId"), "keyup");
     }
 
     ngOnInit() {
@@ -59,7 +58,8 @@ export class WeightingInFormComponent implements OnInit {
             (<FormControl>this.weightingInForm.controls['weight'])
                 .updateValue(_.random(99999), { emitModelToViewChange: true });
         });
-
+        // TODO: แก้ไขการแสดงข้อมูลลูกค้า
+        this._customerService.getCustomer(1).then(customer => this.customer = customer);
     }
 
     onSubmit(data: any) {
