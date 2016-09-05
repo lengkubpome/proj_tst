2ad97fe24b230c6b4abf8e75ef5b47a5e3ebdefb
Kiattiphong Saetang <lengkubpome@hotmail.com>
2016-09-05 13:31:01 +0700

    v1.0.7

--- app/weighting/weight-realtime.component.ts
+++ app/weighting/weight-realtime.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, forwardRef, Input, OnChanges } from '@angular/core';
 import { FormControl, ControlValueAccessor, NG_VALUE_ACCESSOR, NG_VALIDATORS } from '@angular/forms';
-// import {Observable} from 'rxjs/Observable';
+import {Observable} from 'rxjs/Observable';
 
 
 
@@ -23,94 +23,58 @@ export function createCounterRangeValidator(maxValue: any, minValue: any) {
   moduleId: module.id,
   selector: 'weight-realtime',
   template: `
-         
-            <input type="button" (click)="increase()" value="+"> {{counterValue}} <button (click)="decrease()">-</button>       
-<!--          
-            <input type="text" 
-                class="form-control input-lg" 
-                style="text-align: right;font-size: 35px; font-weight: bold;"  
-                name="weight"
-                disabled
-                value="{{weight}}"
-            >
--->
+      <input type="text" 
+        class="form-control input-lg" 
+        style="text-align: right;font-size: 35px; font-weight: bold;"  
+        name="weightIn"
+        disabled
+        value="{{weightIn}}"
+                  >
     `,
   providers: [
     { provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => WeightRealTimeComponent), multi: true },
     { provide: NG_VALIDATORS, useExisting: forwardRef(() => WeightRealTimeComponent), multi: true }
   ]
 })
-export class WeightRealTimeComponent implements OnInit, ControlValueAccessor, OnChanges {
+export class WeightRealTimeComponent implements OnInit, ControlValueAccessor {
 
-  private weight: number;
+  weightIn: number;
+
+  propagateChange: any = () => { };
+  validateFn: any = () => { };
+  // @Input('counterValue') _counterValue = 0;
+  // @Input() counterRangeMax: any;
+  // @Input() counterRangeMin: any;
 
   constructor() { }
 
   ngOnInit() {
-    // let timer = Observable.timer(0, 1000);
-    // timer.subscribe(() => {
-    //     this.weight = _.random(99999);
-    // });
+    let timer = Observable.timer(0, 1000);
+    timer.subscribe(() => {
+      this.weightIn = _.random(99999);
+      this.propagateChange(this.weightIn);
+    });
   }
 
-  //   writeValue(value :any) {
-  //   if (value) {
-  //     this.weight = 99999;
+  // ngOnChanges(inputs: any) {
+  //   if (inputs.counterRangeMax || inputs.counterRangeMin) {
+  //     this.validateFn = createCounterRangeValidator(this.counterRangeMax, this.counterRangeMin);
   //   }
   // }
 
-  // registerOnChange(fn:any) {
-  //   this.propagateChange = fn;
-  // }
-
-  // registerOnTouched() {}
-
-  propagateChange: any = () => { };
-  validateFn: any = () => { };
-  @Input('counterValue') _counterValue = 0;
-  @Input() counterRangeMax: any;
-  @Input() counterRangeMin: any;
-
-  get counterValue() {
-    return this._counterValue;
-
-  }
-
-  set counterValue(val) {
-    this._counterValue = val;
-    this.propagateChange(val);
-  }
-
-  ngOnChanges(inputs: any) {
-    if (inputs.counterRangeMax || inputs.counterRangeMin) {
-      this.validateFn = createCounterRangeValidator(this.counterRangeMax, this.counterRangeMin);
-    }
-  }
-
   writeValue(value: any) {
     if (value) {
-      this.counterValue = value;
-      
-
+      this.weightIn = value;
     }
   }
 
   registerOnChange(fn: any) {
     this.propagateChange = fn;
 
-
   }
 
   registerOnTouched() {
 
-  }
-
-  increase() {
-    this.counterValue++;
-  }
-
-  decrease() {
-    this.counterValue--;
   }
 
   validate(c: FormControl) {
