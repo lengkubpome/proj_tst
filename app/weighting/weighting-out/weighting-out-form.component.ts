79d0dd04418374aaa6fa4adbd832a9f414901cde
Kiattiphong Saetang <lengkubpome@hotmail.com>
2016-09-07 17:28:48 +0700

    v 1.0.9

--- app/weighting/weighting-out/weighting-out-form.component.ts
+++ app/weighting/weighting-out/weighting-out-form.component.ts
@@ -4,9 +4,10 @@ import { WeightingCar } from '../weighting.service';
 @Component({
 
     selector: 'weighting-out',
-     templateUrl: 'app/weighting/weighting-out/weighting-out-form.component.html',
+    templateUrl: 'app/weighting/weighting-out/weighting-out-form.component.html',
+    styleUrls: ['app/weighting/weighting-out/weighting-out-form.component.css']
 })
-export class WeightingOutComponent { 
+export class WeightingOutComponent {
     @Input() car: WeightingCar;
 
 }
